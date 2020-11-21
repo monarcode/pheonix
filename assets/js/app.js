df0a06c37f5c93a078ce6a3949733f54a698e820
Ugbah Isioma <61241222+monarcode@users.noreply.github.com>
2020-11-21 21:15:14 +0100

    fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,4 +13,23 @@ window.onload = (event) => {
     $('body').scrollspy({
         target: '#nav'
     })
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        nav:false,
+        dots:true,
+        autoplay: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
 }
